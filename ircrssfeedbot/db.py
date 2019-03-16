126aca96f9ef2853c360602a8c9a91dcb97d6f99
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-03-16 17:40:56 -0400

    (WIP) Improve database interface

--- ircrssfeedbot/db.py
+++ ircrssfeedbot/db.py
@@ -3,17 +3,18 @@ from typing import List, Optional
 from ircrssfeedbot import config
 
 import peewee
+from peewee import chunked
 
-DATABASE = peewee.SqliteDatabase(None)
+_DATABASE = peewee.SqliteDatabase(None)
 
 
 class Post(peewee.Model):
     channel = peewee.CharField(64, null=False, verbose_name='channel name')
     feed = peewee.CharField(32, null=False, verbose_name='feed name')
-    post = peewee.CharField(12, null=False, verbose_name='post ID')
+    post = peewee.CharField(10, null=False, verbose_name='post ID')
 
     class Meta:
-        database = DATABASE
+        database = _DATABASE
         legacy_table_names = False  # This will become a default in peewee>=4
         primary_key = peewee.CompositeKey('channel', 'feed', 'post')
         indexes = (
@@ -25,17 +26,27 @@ class Post(peewee.Model):
 class Database:
     def __init__(self):
         db_path = '/tmp/sq.db'  # config.INSTANCE['dir'] / config.DB_FILENAME
-        DATABASE.init(db_path)
-        self._db = DATABASE
+        _DATABASE.init(db_path)
+        self._db = _DATABASE
         self._db.create_tables([Post])
 
     def is_new_feed(self, channel: str, feed: str) -> bool:
-        pass
+        conditions = (Post.channel == channel) & (Post.feed == feed)
+        posts = Post.select(Post.post).where(conditions).limit(1).tuples()
+        return not posts
 
-    def find_missing(self, channel: str, feed: Optional[str], posts: List[str]) -> List[str]:
+    def select_missing(self, channel: str, feed: Optional[str], posts: List[str]) -> List[str]:
         if feed:
-            return Post.get(Post.channel == channel, Post.feed == feed, Post.post not in posts)
-        return Post.get(Post.channel == channel, Post.post not in posts)
+            conditions = (Post.channel == channel) & (Post.feed == feed) & Post.post.not_in(posts)
+        else:
+            conditions = (Post.channel == channel) & Post.post.not_in(posts)
+        missing = Post.select(Post.post).where(conditions).tuples().iterator()
+        missing = [post for post in posts if post in missing]  # Sort by original order.
+        return missing
 
     def insert(self, channel: str, feed: str, posts: List[str]) -> None:
-        pass
+        data = ({'channel': channel, 'feed': feed, 'post': post} for post in posts)
+        with self._db.atomic():
+            for batch in chunked(data, 100):
+                Post.insert_many(batch).execute()
+                # Note: Try prepending ".on_conflict_ignore()" before ".execute()" if needed.
