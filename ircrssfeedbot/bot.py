211d1c936c9bbe26870f83142d0dc3a60d91df7a
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-03-17 18:59:40 -0400

    Initialize IRC bot

--- ircrssfeedbot/bot.py
+++ ircrssfeedbot/bot.py
@@ -12,5 +12,17 @@ class Bot:
     def __init__(self) -> None:
         log.info('Initializing bot as: %s', subprocess.check_output('id', text=True).rstrip())
 
-    def serve(self) -> None:
-        pass
+        instance = config.INSTANCE
+        log.info('Alerts will be sent to %s.', instance['alerts_channel'])
+        self._irc = miniirc.IRC(
+            ip=instance['host'],
+            port=instance['ssl_port'],
+            nick=instance['nick'],
+            channels=instance['feeds'],
+            ssl=True,
+            debug=True,
+            ns_identity=f"{instance['nick']} {instance['nick_password']}",
+            connect_modes=instance['mode'],
+            quit_message='',
+            )
+        log.debug('Initialized bot.')
