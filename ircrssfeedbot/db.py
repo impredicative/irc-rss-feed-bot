ac046a48f2b8fd960f937906bdbbd829bd049081
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-03-17 09:33:40 -0400

    Improve database interface

--- ircrssfeedbot/db.py
+++ ircrssfeedbot/db.py
@@ -1,7 +1,7 @@
 import logging
 from pathlib import Path
 import threading
-from typing import List, Optional
+from typing import List, Union
 
 import peewee
 from peewee import chunked
@@ -43,19 +43,28 @@ class Database:
         return not Post.select(Post.post).where(conditions).limit(1)
 
     @staticmethod
-    def select_missing(channel: str, feed: Optional[str], posts: List[str]) -> List[str]:
-        if feed:
-            conditions = (Post.channel == channel) & (Post.feed == feed) & Post.post.not_in(posts)
-        else:
-            conditions = (Post.channel == channel) & Post.post.not_in(posts)
-        missing = Post.select(Post.post).where(conditions).tuples().iterator()
-        missing_ordered = [post for post in posts if post in missing]  # Sort by original order.
-        assert missing == missing_ordered  # TODO: Remove assertion after thoroughly confirming it.
-        return missing_ordered
+    def select_missing(channel: str, feed: Union[str, None], posts: List[str]) -> List[str]:
+        log.debug('Requesting missing posts for channel %s having feed %s out of %s posts.', channel, feed, len(posts))
+        present = []
+        for posts_batch in chunked(posts, 100):  # Ref: https://www.sqlite.org/limits.html#max_variable_number
+            if feed:
+                conditions = (Post.channel == channel) & (Post.feed == feed) & Post.post.in_(posts_batch)
+            else:
+                conditions = (Post.channel == channel) & Post.post.in_(posts_batch)
+            present_batch = Post.select(Post.post).where(conditions).tuples().iterator()
+            present_batch = [post[0] for post in present_batch]
+            present.extend(present_batch)
+        missing = [post for post in posts if post not in present]  # This also implicitly avoids returning duplicates.
+        log.info('Returning %s missing posts for channel %s having feed %s out of %s posts.',
+                 len(missing), channel, feed, len(posts))
+        return missing
 
     def insert(self, channel: str, feed: str, posts: List[str]) -> None:
+        log.debug('Inserting %s posts for channel %s having feed %s.', len(posts), channel, feed)
         data = ({'channel': channel, 'feed': feed, 'post': post} for post in posts)
         with self._write_lock, self._db.atomic():
-            for batch in chunked(data, 100):
+            for batch in chunked(data, 100):  # Ref: https://www.sqlite.org/limits.html#max_variable_number
                 Post.insert_many(batch).execute()
                 # Note: Try prepending ".on_conflict_ignore()" before ".execute()" if needed.
+        log.info('Inserted %s posts for channel %s having feed %s.', len(posts), channel, feed)
+
