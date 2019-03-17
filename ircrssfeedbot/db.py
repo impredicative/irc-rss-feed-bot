664f75a3280f70a5c300cb8473b92a6edc4bc198
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-03-17 17:45:44 -0400

    Rename "post" column to "url"

--- ircrssfeedbot/db.py
+++ ircrssfeedbot/db.py
@@ -1,6 +1,6 @@
 import logging
 import threading
-from typing import List, Union
+from typing import List, Union, Set
 
 import peewee
 from peewee import chunked
@@ -15,22 +15,24 @@ _DATABASE = peewee.SqliteDatabase(None)
 class Post(peewee.Model):
     channel = peewee.CharField(64, null=False, verbose_name='channel name')
     feed = peewee.CharField(32, null=False, verbose_name='feed name')
-    post = peewee.CharField(10, null=False, verbose_name='post ID')
+    url = peewee.CharField(512, null=False, verbose_name='long URL')
+    # Note: The reason for using the "long URL" instead of the "short URL path" is that the latter can change
+    # surprisingly if the array of Bitly tokens is changed in any way, leading to deduplication errors.
 
     class Meta:
         database = _DATABASE
         legacy_table_names = False  # This will become a default in peewee>=4
-        primary_key = peewee.CompositeKey('channel', 'feed', 'post')
+        primary_key = peewee.CompositeKey('channel', 'feed', 'url')
         indexes = (
-            # (('channel', 'feed', 'post'), True),
-            (('channel', 'post'), False),
+            # (('channel', 'feed', 'url'), True),
+            (('channel', 'url'), False),
         )  # True means unique.
 
 
 class Database:
     def __init__(self):
         log.debug('Initializing database.')
-        db_path = config.INSTANCE['dir'] / config.DB_FILENAME
+        db_path = config.INSTANCE.get('dir', config.TEMPDIR) / config.DB_FILENAME
         _DATABASE.init(db_path)  # If facing threading issues, consider https://stackoverflow.com/a/39024742/
         self._db = _DATABASE
         self._db.create_tables([Post])
@@ -38,42 +40,39 @@ class Database:
         log.info('Initialized database having path %s and size %s.', db_path, humanize_bytes(db_path.stat().st_size))
 
     @staticmethod
-    def _select_unposted(conditions: peewee.Expression, posts: List[str]) -> List[str]:
-        present = []
-        for posts_batch in chunked(posts, 100):  # Ref: https://www.sqlite.org/limits.html#max_variable_number
-            conditions &= Post.post.in_(posts_batch)
-            present_batch = Post.select(Post.post).where(conditions).tuples().iterator()
-            present_batch = [post[0] for post in present_batch]
-            present.extend(present_batch)
-        missing = [post for post in posts if post not in present]  # This also implicitly avoids returning duplicates.
-        return missing
+    def _select_unposted(conditions: peewee.Expression, urls: List[str]) -> List[str]:
+        posted: Set[str] = set()
+        for urls_batch in chunked(urls, 100):  # Ref: https://www.sqlite.org/limits.html#max_variable_number
+            conditions_batch = (conditions & Post.url.in_(urls_batch))
+            posted |= {post[0] for post in Post.select(Post.url).where(conditions_batch).tuples().iterator()}
+        unposted = [url for url in urls if url not in posted]
+        return unposted
 
     @staticmethod
     def is_new_feed(channel: str, feed: str) -> bool:
         conditions = (Post.channel == channel) & (Post.feed == feed)
-        return not Post.select(Post.post).where(conditions).limit(1)
+        return not Post.select(Post.url).where(conditions).limit(1)
 
-    def select_unposted_for_channel(self, channel: str, posts: List[str]) -> List[str]:
-        log.debug('Requesting missing posts for channel %s out of %s posts.', channel, len(posts))
+    def select_unposted_for_channel(self, channel: str, urls: List[str]) -> List[str]:
+        log.debug('Requesting unposted URLs for channel %s out of %s.', channel, len(urls))
         conditions = (Post.channel == channel)
-        missing = self._select_unposted(conditions, posts)
-        log.info('Returning %s missing posts for channel %s out of %s posts.', len(missing), channel, len(posts))
-        return missing
+        unposted_urls = self._select_unposted(conditions, urls)
+        log.info('Returning %s unposted URLs for channel %s out of %s.', len(unposted_urls), channel, len(urls))
+        return unposted_urls
 
-    def select_unposted_for_channel_feed(self, channel: str, feed: Union[str, None], posts: List[str]) -> List[str]:
-        log.debug('Requesting missing posts for channel %s having feed %s out of %s posts.', channel, feed, len(posts))
+    def select_unposted_for_channel_feed(self, channel: str, feed: Union[str, None], urls: List[str]) -> List[str]:
+        log.debug('Requesting unposted URLs for channel %s having feed %s out of %s.', channel, feed, len(urls))
         conditions = (Post.channel == channel) & (Post.feed == feed)
-        missing = self._select_unposted(conditions, posts)
-        log.info('Returning %s missing posts for channel %s having feed %s out of %s posts.',
-                 len(missing), channel, feed, len(posts))
-        return missing
+        unposted_urls = self._select_unposted(conditions, urls)
+        log.info('Returning %s unposted URLs for channel %s having feed %s out of %s.',
+                 len(unposted_urls), channel, feed, len(urls))
+        return unposted_urls
 
-    def insert_posted(self, channel: str, feed: str, posts: List[str]) -> None:
-        log.debug('Inserting %s posts for channel %s having feed %s.', len(posts), channel, feed)
-        data = ({'channel': channel, 'feed': feed, 'post': post} for post in posts)
+    def insert_posted(self, channel: str, feed: str, urls: List[str]) -> None:
+        log.debug('Inserting %s URLs for channel %s having feed %s.', len(urls), channel, feed)
+        data = ({'channel': channel, 'feed': feed, 'url': url} for url in urls)
         with self._write_lock, self._db.atomic():
             for batch in chunked(data, 100):  # Ref: https://www.sqlite.org/limits.html#max_variable_number
                 Post.insert_many(batch).execute()
                 # Note: Try prepending ".on_conflict_ignore()" before ".execute()" if needed.
-        log.info('Inserted %s posts for channel %s having feed %s.', len(posts), channel, feed)
-
+        log.info('Inserted %s URLs for channel %s having feed %s.', len(urls), channel, feed)
