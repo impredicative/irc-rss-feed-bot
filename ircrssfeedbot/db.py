74aa470bd5643d2f54619296e3234587206412c7
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-03-17 09:53:55 -0400

    Improve database interface

--- ircrssfeedbot/db.py
+++ ircrssfeedbot/db.py
@@ -38,28 +38,37 @@ class Database:
         self._write_lock = threading.Lock()
         log.info('Initialized database having path %s and size %s.', db_path, humanize_bytes(db_path.stat().st_size))
 
-    def is_new_feed(self, channel: str, feed: str) -> bool:
-        conditions = (Post.channel == channel) & (Post.feed == feed)
-        return not Post.select(Post.post).where(conditions).limit(1)
-
     @staticmethod
-    def select_missing(channel: str, feed: Union[str, None], posts: List[str]) -> List[str]:
-        log.debug('Requesting missing posts for channel %s having feed %s out of %s posts.', channel, feed, len(posts))
+    def _select_unposted(conditions: peewee.Expression, posts: List[str]) -> List[str]:
         present = []
         for posts_batch in chunked(posts, 100):  # Ref: https://www.sqlite.org/limits.html#max_variable_number
-            if feed:
-                conditions = (Post.channel == channel) & (Post.feed == feed) & Post.post.in_(posts_batch)
-            else:
-                conditions = (Post.channel == channel) & Post.post.in_(posts_batch)
+            conditions &= Post.post.in_(posts_batch)
             present_batch = Post.select(Post.post).where(conditions).tuples().iterator()
             present_batch = [post[0] for post in present_batch]
             present.extend(present_batch)
         missing = [post for post in posts if post not in present]  # This also implicitly avoids returning duplicates.
+        return missing
+
+    def is_new_feed(self, channel: str, feed: str) -> bool:
+        conditions = (Post.channel == channel) & (Post.feed == feed)
+        return not Post.select(Post.post).where(conditions).limit(1)
+
+    def select_unposted_for_channel(self, channel: str, posts: List[str]) -> List[str]:
+        log.debug('Requesting missing posts for channel %s out of %s posts.', channel, len(posts))
+        conditions = (Post.channel == channel)
+        missing = self._select_unposted(conditions, posts)
+        log.info('Returning %s missing posts for channel %s out of %s posts.', len(missing), channel, len(posts))
+        return missing
+
+    def select_unposted_for_channel_feed(self, channel: str, feed: Union[str, None], posts: List[str]) -> List[str]:
+        log.debug('Requesting missing posts for channel %s having feed %s out of %s posts.', channel, feed, len(posts))
+        conditions = (Post.channel == channel) & (Post.feed == feed)
+        missing = self._select_unposted(conditions, posts)
         log.info('Returning %s missing posts for channel %s having feed %s out of %s posts.',
                  len(missing), channel, feed, len(posts))
         return missing
 
-    def insert(self, channel: str, feed: str, posts: List[str]) -> None:
+    def insert_posted(self, channel: str, feed: str, posts: List[str]) -> None:
         log.debug('Inserting %s posts for channel %s having feed %s.', len(posts), channel, feed)
         data = ({'channel': channel, 'feed': feed, 'post': post} for post in posts)
         with self._write_lock, self._db.atomic():
