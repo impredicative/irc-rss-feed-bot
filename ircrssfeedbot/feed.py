a95e40eaf7f76e64f537e97df9a339d06b092097
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-03-18 03:06:12 -0400

    Remove URL shortening code

--- ircrssfeedbot/feed.py
+++ ircrssfeedbot/feed.py
@@ -1,30 +1,27 @@
 from dataclasses import dataclass
-import types
 from typing import List
 
-import bitlyshortener
+from descriptors import cachedproperty
 import feedparser
 import requests
 
 from ircrssfeedbot import config
-
-_bitly_shortener = bitlyshortener.Shortener(tokens=config.INSTANCE['tokens']['bitly'],
-                                            max_cache_size=config.BITLY_SHORTENER_MAX_CACHE_SIZE)
 
 
 @dataclass
 class FeedEntry:
     title: str
     long_url: str
-    short_url: str
 
-    @property
-    def id(self) -> str:
-        return self.short_url.rpartition('/')[-1]
+
+@dataclass
+class ShortenedFeedEntry(FeedEntry):
+    short_url: str
 
 
 @dataclass
 class Feed:
+    name: str
     url: str
 
     @property
@@ -33,17 +30,8 @@ class Feed:
         response.raise_for_status()
         return response.content
 
-    @property
+    @cachedproperty
     def entries(self) -> List[FeedEntry]:
         entries = feedparser.parse(self._feed)['entries']
-        entries = [types.SimpleNamespace(title=entry['title'], long_url=entry['link']) for entry in entries]
-        long_urls = [entry.long_url for entry in entries]
-        short_urls = _bitly_shortener.shorten_urls(long_urls)
-        entries = [FeedEntry(title=entry.title, long_url=entry.long_url, short_url=short_urls[i])
-                   for i, entry in enumerate(entries)]
+        entries = [FeedEntry(title=entry['title'], long_url=entry['link']) for entry in entries]
         return entries
-
-
-if __name__ == '__main__':
-    for entry in Feed('https://feeds.feedburner.com/blogspot/gJZg').entries:
-        print(entry)
