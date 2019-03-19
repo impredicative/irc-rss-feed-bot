312ed7812aa6d3886e9fb3dc85b7ca67d6037271
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-03-18 21:40:04 -0400

    Add log messages

--- ircrssfeedbot/feed.py
+++ ircrssfeedbot/feed.py
@@ -1,4 +1,5 @@
 import dataclasses
+import logging
 from typing import List, Union
 
 import bitlyshortener
@@ -8,6 +9,9 @@ import requests
 
 from . import config
 from .db import Database
+from .util.humanize import humanize_len
+
+log = logging.getLogger(__name__)
 
 
 @dataclasses.dataclass
@@ -38,33 +42,46 @@ class Feed:
     url_shortener: bitlyshortener.Shortener = dataclasses.field(repr=False)
 
     def __post_init__(self):
+        log.debug('Initializing instance of %s.', self)
         self._feed_config = config.INSTANCE[self.channel][self.name]
         self._is_new_feed = self.db.is_new_feed(self.channel, self.name)
+        log.debug('Initialized instance of %s.', self)
 
     @property
     def _feed(self) -> bytes:
+        log.debug('Retrieving content for %s.', self)
         response = requests.get(self.url, timeout=config.REQUEST_TIMEOUT, headers={'User-Agent': config.USER_AGENT})
         response.raise_for_status()
-        return response.content
+        content = response.content
+        log.debug('Returning content of size %s for %s.', humanize_len(content), self)
+        return content
 
     @cachedproperty
     def entries(self) -> List[FeedEntry]:
-        return [FeedEntry(title=e['title'], long_url=e['link']) for e in feedparser.parse(self._feed)['entries']]
+        log.debug('Retrieving entries for %s.', self)
+        entries = [FeedEntry(title=e['title'], long_url=e['link']) for e in feedparser.parse(self._feed)['entries']]
+        log.debug('Returning %s entries for %s.', len(entries), self)
+        return entries
 
     @cachedproperty
     def postable_entries(self) -> List[Union[FeedEntry, ShortenedFeedEntry]]:
+        log.debug('Retrieving postable entries for %s.', self)
         entries = self.unposted_entries[:config.MAX_POSTS_OF_NEW_FEED] if self._is_new_feed else self.unposted_entries
 
         # Shorten URLs
         if self._feed_config.get('shorten', True):
+            log.debug('Shortening %s postable long URLs for %s.', len(entries), self)
             long_urls = [entry.long_url for entry in entries]
             short_urls = self.url_shortener.shorten_urls(long_urls)
             entries = [ShortenedFeedEntry(e.title, e.long_url, short_urls[i]) for i, e in enumerate(entries)]
+            log.debug('Shortened %s postable long URLs for %s.', len(entries), self)
 
+        log.debug('Returning %s postable entries for %s.', len(entries), self)
         return entries
 
     @cachedproperty
     def unposted_entries(self) -> List[FeedEntry]:
+        log.debug('Retrieving unposted entries for %s.', self)
         entries = self.entries
         long_urls = [entry.long_url for entry in entries]
         dedup_strategy = self._feed_config.get('dedup', 'feed')
@@ -75,4 +92,5 @@ class Feed:
             long_urls = self.db.select_unposted_for_channel(self.channel, long_urls)
         long_urls = set(long_urls)
         entries = [entry for entry in entries if entry.long_url in long_urls]
+        log.debug('Returning %s unposted entries for %s.', len(entries), self)
         return entries
