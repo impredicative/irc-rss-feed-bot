2b6f5389fc4e7b6de9d5e859160054633bc89967
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-03-25 21:26:25 -0400

    Log levelname before threadName

--- ircrssfeedbot/config.py
+++ ircrssfeedbot/config.py
@@ -33,7 +33,7 @@ LOGGING = {  # Ref: https://docs.python.org/3/howto/logging.html#configuring-log
     'version': 1,
     'formatters': {
         'detailed': {
-            'format': '%(asctime)s %(threadName)s:%(name)s:%(lineno)d:%(funcName)s:%(levelname)s: %(message)s',
+            'format': '%(asctime)s %(levelname)s %(threadName)s:%(name)s:%(lineno)d:%(funcName)s: %(message)s',
         },  # Note: Use %(thread)x- if needed for thread ID.
     },
     'handlers': {
