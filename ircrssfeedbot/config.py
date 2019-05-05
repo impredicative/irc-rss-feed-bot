8388cfed2f6d0d187d773ab4519b185fffbebc50
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-05-04 20:59:18 -0400

    Update user agent to use rv66 instead of rv65

--- ircrssfeedbot/config.py
+++ ircrssfeedbot/config.py
@@ -28,7 +28,7 @@ READ_ATTEMPTS_MAX = 3
 REQUEST_TIMEOUT = 90
 SECONDS_PER_MESSAGE = 2
 TEMPDIR = Path(tempfile.gettempdir())
-USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0'
 
 _DEV = False
 if _DEV:
