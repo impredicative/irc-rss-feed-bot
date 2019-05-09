ac55ffc2582b4f887e7c16c72aa6043ee1d5f31b
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-05-09 14:16:59 -0400

    Print categories too if available

--- scripts/print_feed_entries.py
+++ scripts/print_feed_entries.py
@@ -10,4 +10,8 @@ entries = feedparser.parse(content)['entries']
 
 for index, entry in enumerate(entries):
     title, link = entry['title'], entry['link']
-    print(f'#{index+1}: {title}\n{link}\n')
+    post = f'#{index+1}: {title}\n{link}\n'
+    if hasattr(entry, 'tags') and entry.tags:
+        categories = ', '.join(t['term'] for t in entry.tags)
+        post += f'{categories}\n'
+    print(post)
