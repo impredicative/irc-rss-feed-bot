56811947d9586b623efbd230d4f67c2c08509d52
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-06-03 19:13:43 -0400

    Slightly optimize shorten_to_bytes_width

--- ircrssfeedbot/util/textwrap.py
+++ ircrssfeedbot/util/textwrap.py
@@ -5,7 +5,8 @@ _MIN_WIDTH = 5  # == len(textwrap.shorten(string.ascii_letters, len(string.ascii
 
 def shorten_to_bytes_width(text: str, width: int) -> str:
     # Ref: https://stackoverflow.com/a/56401167/
-    width = max(_MIN_WIDTH, width)  # This prevents ValueError if width < TEXTWRAP_MIN_WIDTH
+    width = max(_MIN_WIDTH, width)  # This prevents ValueError if width < _MIN_WIDTH
+    text = textwrap.shorten(text, width)  # After this line, len(text.encode()) >= width
     while len(text.encode()) > width:
         text = textwrap.shorten(text, len(text) - 1)
     assert len(text.encode()) <= width
