998ccc9fc99b559a759acd6f21239f227b92ae1a
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-06-30 14:28:50 -0400

    Improve style configuration check

--- ircrssfeedbot/util/ircmessage.py
+++ ircrssfeedbot/util/ircmessage.py
@@ -4,4 +4,4 @@ import ircmessage
 
 
 def style(text: str, **style_config: Optional[Dict[str, str]]) -> str:
-    return ircmessage.style(text, **style_config, reset=True) if style_config else text
+    return ircmessage.style(text, **style_config, reset=True) if (style_config and any(style_config.values())) else text
