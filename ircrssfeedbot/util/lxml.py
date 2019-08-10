1347b55d23c8ed524cb760ccdf5b1bec7d36c3e6
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-08-10 19:07:55 -0400

    Avoid storing an unnecessary result in a variable

--- ircrssfeedbot/util/lxml.py
+++ ircrssfeedbot/util/lxml.py
@@ -4,7 +4,7 @@ import lxml.etree
 def sanitize_xml(content: bytes) -> bytes:
     # Ref: https://stackoverflow.com/a/4997458/
     try:
-        root = lxml.etree.fromstring(content)
+        lxml.etree.fromstring(content)
     except lxml.etree.XMLSyntaxError:
         root = lxml.etree.fromstring(content, parser=lxml.etree.XMLParser(recover=True))
         return lxml.etree.tostring(root)
