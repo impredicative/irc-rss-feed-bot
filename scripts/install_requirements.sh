56f3e53252ca324e8b15f405928bcc7f56d8aa99
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-04-12 11:56:40 -0400

    Put pip installations in separate lines

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -3,4 +3,5 @@ set -euxo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}"/..
 
-pip install -U -r ./requirements.txt -r ./requirements-dev.in
+pip install -r ./requirements.txt
+pip install -Ur ./requirements-dev.in
