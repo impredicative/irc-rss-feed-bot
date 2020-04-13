b83d2716475c88356df02e47d819ac8e96ad0a56
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-04-12 22:04:03 -0400

    Simplify parser imports to try to fix Docker Hub build

--- ircrssfeedbot/parsers/__init__.py
+++ ircrssfeedbot/parsers/__init__.py
@@ -1,9 +1,2 @@
 """Import all parsers."""
-from importlib import import_module
-from pathlib import Path
-
-for f in Path(__file__).parent.glob("*.py"):
-    module = f.stem
-    if module != "base":
-        import_module(f".{module}", __package__)
-del import_module, Path
+from . import feedparser, hext, jmespath, pandas
