ffbcf0f3f89345f34a01726a3ca9778caa261b99
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-05-02 10:53:17 -0400

    Include "wheel" as dev requirement due to errors while installing requirements

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}"/..
 
-pip install -r ./requirements.txt
 pip install -Ur ./requirements-dev.in
+pip install -r ./requirements.txt
