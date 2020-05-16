174cf9ad72d05e9ffe0353d593880bfe6a75f9df
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-05-16 16:17:49 -0400

    Working changes to feed testing script

--- scripts/print_feed_entries.py
+++ scripts/print_feed_entries.py
@@ -8,6 +8,9 @@ Customize CHANNEL and FEED.
 """
 
 import logging
+import os
+
+import bitlyshortener
 
 from ircrssfeedbot import config
 from ircrssfeedbot.__main__ import load_instance_config
@@ -22,7 +25,7 @@ FEED = "stats:🇺🇸"  # USA
 # FEED = "stats:🇷🇺"  # Russia
 # FEED = "stats:🇨🇳"  # China
 # FEED = "COVID-19:stats:USA:NY"
-CHANNEL, FEED = "##CoV", "MX" or "SSRN"
+CHANNEL, FEED = "##CoV", "LitCovid:TSV"
 
 config.LOGGING["loggers"][config.PACKAGE_NAME]["level"] = "DEBUG"  # type: ignore
 config.configure_logging()
@@ -34,10 +37,15 @@ config.runtime.identity = ""
 load_instance_config(log_details=False)
 config.INSTANCE["feeds"][CHANNEL][FEED]["style"] = None
 
-feed = Feed(channel=CHANNEL, name=FEED, db=None, url_shortener=None)  # type: ignore
+_url_shortener = bitlyshortener.Shortener(
+    tokens=[token.strip() for token in os.environ["BITLY_TOKENS"].strip().split(",")],
+    max_cache_size=config.BITLY_SHORTENER_MAX_CACHE_SIZE,
+)
+
+feed = Feed(channel=CHANNEL, name=FEED, db=None, url_shortener=_url_shortener)  # type: ignore
 # feed = Feed(channel=CHANNEL, name=FEED, db=None, url_shortener=None)  # type: ignore
-for index, entry in enumerate(feed.entries):
-    post = f"\n#{index + 1}: {entry.message}"
+for index, entry in enumerate(feed.entries[:100]):
+    post = f"\n#{index + 1:,}: {entry.message}"
     if entry.categories:
         post += "\nCategories: " + ", ".join(entry.categories)
     print(post)
