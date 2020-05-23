302164c4c4cbce297c995fccc8c2a37cc9e5d1f3
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-05-22 23:34:45 -0400

    Pin toml==0.10.0 to prevent incompatibility of dressup==0.1.0 with toml==0.10.1

--- ircrssfeedbot/style.py
+++ ircrssfeedbot/style.py
@@ -2,10 +2,13 @@
 
 The supported stylers are: asterisk, irc, unicode
 """
+import logging
 from typing import Any, Callable, Dict, Union
 
 import dressup
 import ircmessage
+
+log = logging.getLogger(__name__)
 
 
 def _asterisk_style(text: str, **_kwargs: Any) -> str:
@@ -16,12 +19,19 @@ def _dressup_style(text: str, bold: bool = False, italics: bool = False) -> str:
     """Style the given text with the given options using `dressup`."""
     if not (bold or italics):
         return text
+
+    # Define unicode type
     unicode_type = "math sans"
     if bold:
         unicode_type += " bold"
     if italics:
         unicode_type += " italic"
-    return dressup.convert(text, unicode_type=unicode_type)
+
+    # Style
+    try:
+        return dressup.convert(text, unicode_type=unicode_type)
+    except Exception as exc:
+        log.exception(f"Error using dressup with {unicode_type=} to format {text!r}: {exc}")
 
 
 def _ircmessage_style(text: str, **style_config: Union[bool, str]) -> str:
