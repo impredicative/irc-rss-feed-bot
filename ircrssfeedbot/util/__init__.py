f82ddefa579df0a6032964abf7a9a452769644b9
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-05-26 23:38:57 -0400

    Remove unmaintained comment

--- ircrssfeedbot/util/__init__.py
+++ ircrssfeedbot/util/__init__.py
@@ -6,6 +6,3 @@ for f in Path(__file__).parent.glob("*.py"):
     if not f.stem.startswith("_"):
         import_module(f".{f.stem}", __package__)
 del import_module, Path
-
-# # pylint: disable=redefined-builtin
-# from . import datetime, float, hashlib, hext, humanize, ircmessage, list, lxml, set, textwrap, timeit, urllib
