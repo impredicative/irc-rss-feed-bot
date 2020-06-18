b547ff4d4f9cdd9a7f99838e84a82c3d5f5ecf02
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-06-17 20:05:52 -0400

    Improve commit and log message of GitHub publisher

--- ircrssfeedbot/publishers/github.py
+++ ircrssfeedbot/publishers/github.py
@@ -8,6 +8,7 @@ from typing import Any, Dict
 import github
 import pandas as pd
 
+from ..util.str import readable_list
 from ._base import BasePublisher
 
 
@@ -22,12 +23,13 @@ class Publisher(BasePublisher):
     def _publish(self, channel: str, df_entries: pd.DataFrame) -> Dict[str, Any]:
         assert not df_entries.empty
         path = f"{channel}/{datetime.datetime.utcnow().strftime('%Y/%m%d/%H%M%S')}.csv"  # Ref: https://strftime.org/
+        feed_counts = readable_list([f"{count} {value}" for value, count in df_entries["feed"].value_counts().iteritems()])
         content = df_entries.drop(columns="channel").to_csv(index=False)
-        self._repo.create_file(path=path, message=f"Add {len(df_entries)} entries for {channel}", content=content)
+        self._repo.create_file(path=path, message=f"Add {feed_counts} entries of {channel}", content=content)
         return {
             "path": path,
-            "content_len": len(content),
+            # "content_len": len(content),
             "rate_remaining": self._github.rate_limiting[0],
-            "rate_limit": self._github.rate_limiting[1],
+            # "rate_limit": self._github.rate_limiting[1],  # Always 5000.
             "rate_reset": datetime.timedelta(seconds=round(self._github.rate_limiting_resettime - time.time())),
         }
