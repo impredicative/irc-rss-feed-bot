9f77c08f6c23d192e29d6f8ee57cf36767605a10
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-07-27 20:02:55 -0400

    Revert to always using spawn start method due to freeze-ups

--- ircrssfeedbot/__main__.py
+++ ircrssfeedbot/__main__.py
@@ -1,10 +1,8 @@
 """Package entrypoint."""
 import multiprocessing
-import os
 
 if __name__ == "__main__":
-    if os.getenv("IRCRSSFEEDBOT_TRACEMALLOC"):
-        multiprocessing.set_start_method("spawn")
+    multiprocessing.set_start_method("spawn")
     from ircrssfeedbot.main import main
 
     main()
