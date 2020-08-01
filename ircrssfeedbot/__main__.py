9df291b5edfa662db820d3900b3b0a4869fb6c1c
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-08-01 13:01:33 -0400

    Add comment about use of spawn start method

--- ircrssfeedbot/__main__.py
+++ ircrssfeedbot/__main__.py
@@ -2,7 +2,7 @@
 import multiprocessing
 
 if __name__ == "__main__":
-    multiprocessing.set_start_method("spawn")
+    multiprocessing.set_start_method("spawn")  # Prevents freeze-up after 1 to 2 days of running
     from ircrssfeedbot.main import main
 
     main()
