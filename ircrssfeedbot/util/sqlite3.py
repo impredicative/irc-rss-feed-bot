f5b6a70aedeaad669313fa3cf494264d74a40863
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-09-21 08:43:56 -0400

    Improve support for NOT in search

--- ircrssfeedbot/util/sqlite3.py
+++ ircrssfeedbot/util/sqlite3.py
@@ -41,7 +41,9 @@ class SqliteFTS5Matcher:
         with contextlib.redirect_stdout(io.StringIO()):  # Workaround for https://github.com/jurismarches/luqum/issues/57
             tree = _UNKNOWN_OP_RESOLVER(parser.parse(query))
         tree = _SearchFieldRemover().visit(tree)
-        return str(tree)
+        query = str(tree)
+        query = query.replace(" AND NOT ", " NOT ")  # Approximate workaround for sqlite3.OperationalError: fts5: syntax error near "NOT"
+        return query
 
     def is_match(self, text: str) -> bool:
         """Return whether the given text string is a match for the query."""
@@ -50,4 +52,8 @@ class SqliteFTS5Matcher:
         cursor = db.cursor()
         cursor.execute("CREATE VIRTUAL TABLE t USING fts5(c);")
         cursor.execute("INSERT INTO t VALUES(?);", (text,))
-        return bool(cursor.execute("SELECT * FROM t WHERE t MATCH ?;", (self._query,)).fetchall())
+        try:
+            results_cursor = cursor.execute("SELECT * FROM t WHERE t MATCH ?;", (self._query,))
+        except sqlite3.OperationalError as exception:
+            raise sqlite3.OperationalError(f"{exception} (with query {self._query!r})")
+        return bool(results_cursor.fetchall())
