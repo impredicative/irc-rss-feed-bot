d329024e45c3553064d6e02d2ccb8f693f0930af
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2022-12-06 17:11:06 +0000

    Fix script to run bot

--- scripts/run_bot.sh
+++ scripts/run_bot.sh
@@ -5,10 +5,10 @@
 # This script is intended for developer testing purposes. Actual use is advised via Docker as documented in the readme.
 #
 # Usage:
-# Customize IRC_PASSWORD below, but do not checkin the password into version control.
+# Customize the values of the variables below, but do not check-in IRC_PASSWORD into version control.
 # Run: ./scripts/run_bot.sh
 
-IRC_PASSWORD=YourActualPassword
+export IRC_PASSWORD=YourActualPassword
 CONFIG_PATH="/workspaces/irc-bots/libera/feed-bot/config.yaml"
 
 python -m ircrssfeedbot --config-path "${CONFIG_PATH}"
