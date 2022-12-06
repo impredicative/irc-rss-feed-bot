7f351fda443d894ffea11e849c36145b9108bb5e
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2022-12-06 17:06:58 +0000

    Add comment section to run_bot script

--- scripts/run_bot.sh
+++ scripts/run_bot.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+# Run the bot.
+#
+# This script is intended for developer testing purposes. Actual use is advised via Docker as documented in the readme.
+#
+# Usage:
+# Customize IRC_PASSWORD below, but do not checkin the password into version control.
+# Run: ./scripts/run_bot.sh
+
 IRC_PASSWORD=YourActualPassword
 CONFIG_PATH="/workspaces/irc-bots/libera/feed-bot/config.yaml"
 
